b2115a84f88e0453de6fdd502c9826b03c851a84
Bret Jordan <jordan2175@gmail.com>
2015-10-16 14:26:20 -0600

    refactored code to change layer 2 addresses

--- rewritecap.go
+++ rewritecap.go
@@ -8,14 +8,12 @@ package main
 
 import (
 	"code.google.com/p/getopt"
-	"encoding/hex"
 	"fmt"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 	"github.com/google/gopacket/pcapgo"
 	"net"
 	"os"
-	"strings"
 	"time"
 )
 
@@ -33,7 +31,7 @@ var iOptNewDay = getopt.IntLong("day", 'd', 0, "Rebase to Day (dd)", "int")
 var bOptHelp = getopt.BoolLong("help", 0, "Help")
 var bOptVer = getopt.BoolLong("version", 0, "Version")
 
-var iDebug = 0
+var iDebug = 1
 var sVersion = "1.40"
 
 //
@@ -46,16 +44,7 @@ func main() {
 	getopt.HelpColumn = 25
 	getopt.SetParameters("")
 	getopt.Parse()
-
-	if *bOptVer {
-		fmt.Println("Version:", sVersion)
-		os.Exit(0)
-	}
-
-	if *bOptHelp || (*sOptPcapSrcFilename == "" || *sOptPcapNewFilename == "") {
-		getopt.Usage()
-		os.Exit(0)
-	}
+	checkCommandLineOptions()
 
 	// Figure out if there is a change needed for the date of each packet.  We will
 	// compute the difference between what is in the first packet and what was passed
@@ -63,7 +52,8 @@ func main() {
 	iDiffYear, iDiffMonth, iDiffDay := computeNeededPacketDateChange()
 
 	// Parse layer 2 addresses
-	userSuppliedMacAddress, userSuppliedMacAddressNew := parseSuppliedLayer2Addresses()
+	userSuppliedMacAddress := parseSuppliedLayer2Address(sOptMacAddress)
+	userSuppliedMacAddressNew := parseSuppliedLayer2Address(sOptMacAddressNew)
 
 	// Parse layer 3 IPv4 address
 	userSuppliedIPv4Address, userSuppliedIPv4AddressNew := parseSuppliedLayer3IPv4Addresses()
@@ -122,40 +112,9 @@ func main() {
 			packet.Metadata().CaptureInfo.Timestamp = tsNew
 		} // End update date stamp
 
-		//
-		//
-		//
-		// Lets compare the mac address supplied with the one in the pcap file for both
-		// the DST MAC and SRC MAC but only if a MAC address is supplied as an ARG
-		// This change will be made regardless of packet type
 		if *sOptMacAddress != "" && *sOptMacAddressNew != "" {
-			dstMacAddressFromPacket := packet.LinkLayer().LayerContents()[0:6]
-			srcMacAddressFromPacket := packet.LinkLayer().LayerContents()[6:12]
-
-			bDstMacAddressMatch := areByteSlicesEqual(dstMacAddressFromPacket, userSuppliedMacAddress)
-			if bDstMacAddressMatch {
-				if iDebug == 1 {
-					fmt.Println("DEBUG: There is a match on the DST MAC Address, updating", makePrettyMacAddress(userSuppliedMacAddress), "to", makePrettyMacAddress(userSuppliedMacAddressNew))
-				}
-
-				for i := 0; i < 6; i++ {
-					packet.LinkLayer().LayerContents()[i] = userSuppliedMacAddressNew[i]
-				}
-			}
-
-			bSrcMacAddressMatch := areByteSlicesEqual(srcMacAddressFromPacket, userSuppliedMacAddress)
-			if bSrcMacAddressMatch {
-				if iDebug == 1 {
-					fmt.Println("DEBUG: There is a match on the SRC MAC Address, updating", makePrettyMacAddress(userSuppliedMacAddress), "to", makePrettyMacAddress(userSuppliedMacAddressNew))
-				}
-
-				j := 0
-				for i := 6; i < 12; i++ {
-					packet.LinkLayer().LayerContents()[i] = userSuppliedMacAddressNew[j]
-					j++
-				}
-			}
-		} // End Update Layer 2 MAC Addresses
+			replaceMacAddresses(packet, userSuppliedMacAddress, userSuppliedMacAddressNew)
+		}
 
 		i802dot1QOffset := 0
 
@@ -337,7 +296,27 @@ func main() {
 } // main()
 
 //
-//
+// --------------------------------------------------------------------------------
+// checkCommandLineOptions()
+// --------------------------------------------------------------------------------
+// Verify that all of the command line options meet the required dependencies
+func checkCommandLineOptions() {
+	if *bOptVer {
+		fmt.Println("Version:", sVersion)
+		os.Exit(0)
+	}
+
+	if *bOptHelp || (*sOptPcapSrcFilename == "" || *sOptPcapNewFilename == "") {
+		getopt.Usage()
+		os.Exit(0)
+	}
+
+	if (*sOptMacAddress != "" && *sOptMacAddressNew == "") || (*sOptMacAddressNew != "" && *sOptMacAddress == "") {
+		getopt.Usage()
+		os.Exit(0)
+	}
+}
+
 //
 // --------------------------------------------------------------------------------
 // computeNeededPacketDateChange()
@@ -367,53 +346,6 @@ func computeNeededPacketDateChange() (iDiffYear, iDiffMonth, iDiffDay int) {
 	}
 	return
 } // computeNeededPacketDateChange
-
-//
-//
-//
-// --------------------------------------------------------------------------------
-// parseSuppliedLayer3Addresses
-// --------------------------------------------------------------------------------
-// Figure out if we need to change a layer 2 mac address
-func parseSuppliedLayer2Addresses() (userSuppliedMacAddress, userSuppliedMacAddressNew []byte) {
-	userSuppliedMacAddress = make([]byte, 6, 6)
-	userSuppliedMacAddressNew = make([]byte, 6, 6)
-
-	// Make sure if the user supplies one Layer2 option, that they also supply the other
-	if (*sOptMacAddress != "" && *sOptMacAddressNew == "") || (*sOptMacAddressNew != "" && *sOptMacAddress == "") {
-		getopt.Usage()
-		os.Exit(0)
-	}
-
-	if *sOptMacAddress != "" {
-		var err error
-		userSuppliedMacAddress, err = net.ParseMAC(*sOptMacAddress)
-
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(0)
-		}
-
-		if iDebug == 1 {
-			fmt.Println("DEBUG: Passed in MAC Address to Change", *sOptMacAddress)
-			fmt.Println("DEBUG: Parsed MAC Address to", userSuppliedMacAddress)
-		}
-	}
-	if *sOptMacAddressNew != "" {
-		var err error
-		userSuppliedMacAddressNew, err = net.ParseMAC(*sOptMacAddressNew)
-
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(0)
-		}
-		if iDebug == 1 {
-			fmt.Println("DEBUG: Passed in new MAC Address", *sOptMacAddressNew)
-			fmt.Println("DEBUG: Parsed new MAC Address to", userSuppliedMacAddressNew)
-		}
-	}
-	return
-} // parseSuppliedLayer2Addresses
 
 //
 //
@@ -476,32 +408,6 @@ func getFirstPacketTimestamp(sFilename string) time.Time {
 	}
 	return ts
 } // getFirstPacketTimestamp
-
-//
-//
-//
-// --------------------------------------------------------------------------------
-//  makePrettyMacAddress
-// --------------------------------------------------------------------------------
-// This function will create a human readable MAC address in upper case using
-// the : notation between octets
-func makePrettyMacAddress(mac []byte) string {
-	sMAC := strings.ToUpper(hex.EncodeToString(mac))
-	var sNewMAC string
-
-	// This will add a ":" after ever ODD index value but not on the last one
-	for i, value := range sMAC {
-		sNewMAC += string(value)
-		if i%2 != 0 && i%11 != 0 {
-			sNewMAC += ":"
-		}
-	}
-	if iDebug == 1 {
-		fmt.Println("DEBUG: MAC Address", sNewMAC)
-	}
-
-	return sNewMAC
-} // makePrettyMacAddress
 
 //
 //
